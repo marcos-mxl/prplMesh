600e958c470f975a8f42fd2617a38a1bf10bdaca
Tomer Eliyahu <tomer.b.eliyahu@intel.com>
2019-07-30 00:31:32 +0300

    WIP - docker compose support
    Signed-off-by: Tomer Eliyahu <tomer.b.eliyahu@intel.com>

---
+++ tools/docker/tests/entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+###############################################################
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+# Copyright (c) 2019 Tomer Eliyahu (Intel)
+# This code is subject to the terms of the BSD+Patent license.
+# See LICENSE file for more details.
+###############################################################
+
+run() {
+    echo "$*"
+    "$@" || exit $?
+}
+
+run ip link add br-lan type bridge
+run ip link add wlan0 type dummy
+run ip link add wlan2 type dummy
+run ip link add sim-eth0 type dummy
+run ip link set sim-eth0 master br-lan
+run ip link set eth0 master br-lan
+run ip link set wlan0 master br-lan
+run ip link set wlan2 master br-lan
+run ip address flush dev eth0
+run ip link set dev sim-eth0 up
+run ip link set dev wlan0 up
+run ip link set dev wlan2 up
+run ip address add dev br-lan ${1}
+run ip link set dev br-lan up
+
+shift
+
+cd ${INSTALL_DIR}
+exec /bin/bash "$@"
