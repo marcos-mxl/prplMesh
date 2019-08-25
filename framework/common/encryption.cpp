4dea7fcf23ab46dfb901ac66956d9d85e4fe5a79
Tomer Eliyahu <tomer.b.eliyahu@intel.com>
2019-08-26 01:09:47 +0300

    Add bbg crypto code to prplMesh
    Signed-off-by: Tomer Eliyahu <tomer.b.eliyahu@intel.com>

--- framework/common/encryption.cpp
+++ framework/common/encryption.cpp
@@ -420,5 +420,325 @@ bool kwa_compute(const uint8_t *authkey, uint8_t *data, uint32_t data_len, uint8
     return true;
 }
 
+// bbf reference code
+
+////////////////////////////////////////////////////////////////////////////////
+// Platform API: Interface related functions to be used by platform-independent
+// files (functions declarations are  found in "../interfaces/platform.h)
+////////////////////////////////////////////////////////////////////////////////
+
+INT8U PLATFORM_GET_RANDOM_BYTES(INT8U *p, INT16U len)
+{
+    FILE   *fd;
+    INT32U  rc;
+
+    fd = fopen("/dev/urandom", "rb");
+
+    if (NULL == fd)
+    {
+        PLATFORM_PRINTF_DEBUG_WARNING("[PLATFORM] Cannot open /dev/urandom\n");
+        return 0;
+    }
+
+    rc = fread(p, 1, len, fd);
+
+    fclose(fd);
+
+    if (len != rc)
+    {
+        PLATFORM_PRINTF_DEBUG_WARNING("[PLATFORM] Could not obtain enough random bytes\n");
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+INT8U PLATFORM_GENERATE_DH_KEY_PAIR(INT8U **priv, INT16U *priv_len, INT8U **pub, INT16U *pub_len)
+{
+    DH *dh;
+
+    if (
+         NULL == priv     ||
+         NULL == priv_len ||
+         NULL == pub      ||
+         NULL == pub_len
+       )
+    {
+        return 0;
+    }
+
+    if (NULL == (dh = DH_new()))
+    {
+        return 0;
+    }
+
+    // Convert binary to BIGNUM format
+    //
+    if (NULL == (dh->p = BN_bin2bn(dh1536_p,sizeof(dh1536_p),NULL)))
+    {
+        DH_free(dh);
+        return 0;
+    }
+    if (NULL == (dh->g = BN_bin2bn(dh1536_g,sizeof(dh1536_g),NULL)))
+    {
+        DH_free(dh);
+        return 0;
+    }
+
+    // Obtain key pair
+    //
+    if (0 == DH_generate_key(dh))
+    {
+        DH_free(dh);
+        return 0;
+    }
+
+    *priv_len = BN_num_bytes(dh->priv_key);
+    *priv     = (INT8U *)malloc(*priv_len);
+    BN_bn2bin(dh->priv_key, *priv);
+
+    *pub_len = BN_num_bytes(dh->pub_key);
+    *pub     = (INT8U *)malloc(*pub_len);
+    BN_bn2bin(dh->pub_key, *pub);
+
+    DH_free(dh);
+      // NOTE: This internally frees "dh->p" and "dh->q", thus no need for us
+      // to do anything else.
+
+    return 1;
+}
+
+INT8U PLATFORM_COMPUTE_DH_SHARED_SECRET(INT8U **shared_secret, INT16U *shared_secret_len, INT8U *remote_pub, INT16U remote_pub_len, INT8U *local_priv, INT8U local_priv_len)
+{
+    BIGNUM *pub_key;
+
+    size_t rlen;
+    int    keylen;
+
+    DH *dh;
+
+    if (
+         NULL == shared_secret     ||
+         NULL == shared_secret_len ||
+         NULL == remote_pub        ||
+         NULL == local_priv
+       )
+    {
+        return 0;
+    }
+
+    if (NULL == (dh = DH_new()))
+    {
+        return 0;
+    }
+
+    // Convert binary to BIGNUM format
+    //
+    if (NULL == (dh->p = BN_bin2bn(dh1536_p,sizeof(dh1536_p),NULL)))
+    {
+        DH_free(dh);
+        return 0;
+    }
+    if (NULL == (dh->g = BN_bin2bn(dh1536_g,sizeof(dh1536_g),NULL)))
+    {
+        DH_free(dh);
+        return 0;
+    }
+    if (NULL == (pub_key = BN_bin2bn(remote_pub, remote_pub_len, NULL)))
+    {
+        DH_free(dh);
+        return 0;
+    }
+    if (NULL == (dh->priv_key = BN_bin2bn(local_priv, local_priv_len, NULL)))
+    {
+        BN_clear_free(pub_key);
+        DH_free(dh);
+        return 0;
+    }
+
+    // Allocate output buffer
+    //
+    rlen            = DH_size(dh);
+    *shared_secret  = (INT8U*)malloc(rlen);
+
+    // Compute the shared secret and save it in the output buffer
+    //
+    keylen = DH_compute_key(*shared_secret, pub_key, dh);
+    if (keylen < 0)
+    {
+        *shared_secret_len = 0;
+        free(*shared_secret);
+        *shared_secret = NULL;
+        BN_clear_free(pub_key);
+        DH_free(dh);
+
+        return 0;
+    }
+    else
+    {
+        *shared_secret_len = (INT16U)keylen;
+    }
+
+    BN_clear_free(pub_key);
+    DH_free(dh);
+
+    return 1;
+}
+
+INT8U PLATFORM_SHA256(INT8U num_elem, INT8U **addr, INT32U *len, INT8U *digest)
+{
+    INT8U res;
+    unsigned int  mac_len;
+    EVP_MD_CTX   *ctx;
+
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L
+    ctx = EVP_MD_CTX_new();
+    if (!ctx)
+    {
+        return 0;
+    }
+#else
+    EVP_MD_CTX  ctx_aux;
+    ctx = &ctx_aux;
+
+    EVP_MD_CTX_init(ctx);
+#endif
+
+    res = 1;
+
+    if (!EVP_DigestInit_ex(ctx, EVP_sha256(), NULL))
+    {
+        res = 0;
+    }
+
+    if (1 == res)
+    {
+        size_t i;
+
+        for (i = 0; i < num_elem; i++)
+        {
+            if (!EVP_DigestUpdate(ctx, addr[i], len[i]))
+            {
+                res = 0;
+                break;
+            }
+        }
+    }
+
+    if (1 == res)
+    {
+        if (!EVP_DigestFinal(ctx, digest, &mac_len))
+        {
+            res = 0;
+        }
+    }
+
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L
+    EVP_MD_CTX_free(ctx);
+#endif
+
+    return res;
+}
+
+INT8U PLATFORM_HMAC_SHA256(INT8U *key, INT32U keylen, INT8U num_elem, INT8U **addr, INT32U *len, INT8U *hmac)
+{
+    HMAC_CTX *ctx;
+    size_t    i;
+
+    unsigned int mdlen = 32;
+
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L
+    ctx = HMAC_CTX_new();
+    if (!ctx)
+    {
+        return 0;
+    }
+#else
+    HMAC_CTX  ctx_aux;
+    ctx = &ctx_aux;
+
+    HMAC_CTX_init(ctx);
+#endif
+
+    HMAC_Init_ex(ctx, key, keylen, EVP_sha256(), NULL);
+
+    for (i = 0; i < num_elem; i++)
+    {
+        HMAC_Update(ctx, addr[i], len[i]);
+    }
+
+    HMAC_Final(ctx, hmac, &mdlen);
+
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L
+    HMAC_CTX_free(ctx);
+#else
+    HMAC_CTX_cleanup(ctx);
+#endif
+
+    return 1;
+}
+
+INT8U PLATFORM_AES_ENCRYPT(INT8U *key, INT8U *iv, INT8U *data, INT32U data_len)
+{
+    EVP_CIPHER_CTX ctx;
+
+    int clen, len;
+    INT8U buf[AES_BLOCK_SIZE];
+
+    EVP_CIPHER_CTX_init(&ctx);
+    if (EVP_EncryptInit_ex(&ctx, EVP_aes_128_cbc(), NULL, key, iv) != 1)
+    {
+        return 0;
+    }
+    EVP_CIPHER_CTX_set_padding(&ctx, 0);
+
+    clen = data_len;
+    if (EVP_EncryptUpdate(&ctx, data, &clen, data, data_len) != 1 || clen != (int) data_len)
+    {
+        return 0;
+    }
+
+    len = sizeof(buf);
+    if (EVP_EncryptFinal_ex(&ctx, buf, &len) != 1 || len != 0)
+    {
+        return 0;
+    }
+    EVP_CIPHER_CTX_cleanup(&ctx);
+
+    return 1;
+}
+
+INT8U PLATFORM_AES_DECRYPT(INT8U *key, INT8U *iv, INT8U *data, INT32U data_len)
+{
+    EVP_CIPHER_CTX ctx;
+
+    int plen, len;
+    INT8U buf[AES_BLOCK_SIZE];
+
+    EVP_CIPHER_CTX_init(&ctx);
+    if (EVP_DecryptInit_ex(&ctx, EVP_aes_128_cbc(), NULL, key, iv) != 1)
+    {
+        return 0;
+    }
+    EVP_CIPHER_CTX_set_padding(&ctx, 0);
+
+    plen = data_len;
+    if (EVP_DecryptUpdate(&ctx, data, &plen, data, data_len) != 1 || plen != (int) data_len)
+    {
+        return 0;
+    }
+
+    len = sizeof(buf);
+    if (EVP_DecryptFinal_ex(&ctx, buf, &len) != 1 || len != 0)
+    {
+        return 0;
+    }
+    EVP_CIPHER_CTX_cleanup(&ctx);
+
+    return 1;
+}
+
 } // namespace encryption
 } // namespace mapf
--- framework/common/include/mapf/common/encryption.h
+++ framework/common/include/mapf/common/encryption.h
@@ -148,6 +148,96 @@ bool wps_calculate_keys(const diffie_hellman &dh, const uint8_t *remote_pubkey,
                         unsigned remote_pubkey_length, const uint8_t *m1_nonce, const uint8_t *mac,
                         const uint8_t *m2_nonce, uint8_t *authkey, uint8_t *keywrapkey);
 
+// BBF CODE
+
+#define INT8U unsigned char
+#define INT16U unsigned short int
+#define INT32U unsigned int
+#define INT8S signed char
+#define INT16S signed short int
+#define INT32S signed int
+#define PLATFORM_PRINTF_DEBUG_WARNING(x) LOG(DEBUG) << x
+#define SHA256_MAC_LEN 32
+#define AES_BLOCK_SIZE 16
+
+// Fill the buffer of length 'len' pointed by 'p' with random bytes.
+//
+// Return "0" if there was a problem, "1" otherwise
+//
+INT8U PLATFORM_GET_RANDOM_BYTES(INT8U *p, INT16U len);
+
+
+// Return a Diffie Hellman pair of private and public keys (and its lengths) in
+// the output arguments "priv", "priv_len", "pub" and "pub_len".
+//
+// Both "priv" and "pub" must be deallocated by the caller when they are no
+// longer needed (using "PLATFORM_FREE()")
+//
+// The keys are obtained using the DH group specified in RFC3523 "section 2"
+// (ie. the "1536-bit MODP Group" where "g = 2" and "p = 2^1536 - 2^1472 - 1 +
+// 2^64 * { [2^1406 pi] + 741804 }")
+//
+// Return "0" if there was a problem, "1" otherwise
+//
+INT8U PLATFORM_GENERATE_DH_KEY_PAIR(INT8U **priv, INT16U *priv_len, INT8U **pub, INT16U *pub_len);
+
+// Return the Diffie Hell shared secret (in output argument "shared_secret"
+// which is "shared_secret_len" bytes long) associated to a remote public key
+// ("remote_pub", which is "remote_pub_len" bytes long") and a local private
+// key ("local_priv", which is "local_priv_len" bytes long).
+//
+// "shared_secret" must be deallocated by the caller once it is no longer needed
+// (using "PLATFORM_FREE()")
+//
+// Return "0" if there was a problem, "1" otherwise
+//
+INT8U PLATFORM_COMPUTE_DH_SHARED_SECRET(INT8U **shared_secret, INT16U *shared_secret_len, INT8U *remote_pub, INT16U remote_pub_len, INT8U *local_priv, INT8U local_priv_len);
+
+// Return the SHA256 digest of the provided input.
+//
+// The provided input is the result of concatenating 'num_elem' elements
+// (addr[0], addr[1], ..., addr[num_elem-1] of size len[0], len[1], ...,
+// len[num_elem-1])
+//
+// The digest is returned in the 'digest' output argument which must point to
+// a preallocated buffer of "SHA256_MAC_LEN" bytes.
+//
+INT8U PLATFORM_SHA256(INT8U num_elem, INT8U **addr, INT32U *len, INT8U *digest);
+
+
+// Return the HMAC_SHA256 digest of the provided input using the provided 'key'
+// (which is 'keylen' bytes long).
+//
+// The provided input is the result of concatenating 'num_elem' elements
+// (addr[0], addr[1], ..., addr[num_elem-1] of size len[0], len[1], ...,
+// len[num_elem-1])
+//
+// The digest is returned in the 'hmac' output argument which must point to
+// a preallocated buffer of "SHA256_MAC_LEN" bytes.
+//
+INT8U PLATFORM_HMAC_SHA256(INT8U *key, INT32U keylen, INT8U num_elem, INT8U **addr, INT32U *len, INT8U *hmac);
+
+// Encrypt the provided 'data' (which is a pointer to buffer of size
+// n*AES_BLOCK_SIZE) using the AES 128 CBC algorithm with the provided
+// "initialization vector" ('iv', which is also a pointer to a buffer of
+// AES_BLOCK_SIZE bytes).
+//
+// The result is written to THE SAME 'data' buffer and has the same length as
+// the input (plain) data.
+//
+// Note that you might have to "pad" the data buffer (so that its length is a
+// multiple of AES_BLOCK_SIZE) in most cases.
+//
+// Return "0" if there was a problem, "1" otherwise
+//
+INT8U PLATFORM_AES_ENCRYPT(INT8U *key, INT8U *iv, INT8U *data, INT32U data_len);
+
+// Works exactly like "PLATFORM_AES_ENCRYPT", but now the 'data' buffer
+// originally contains encrypted data and after the call it contains
+// unencrypted data.
+INT8U PLATFORM_AES_DECRYPT(INT8U *key, INT8U *iv, INT8U *data, INT32U data_len);
+
+
 } // namespace encryption
 } // namespace mapf
 #endif // ENCRYPTION_H_
